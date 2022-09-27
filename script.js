660832cff10a9b18e7bfacb4289efd188ae2cead
WLAMurat <mikhail@thewhitelabelagency.com>
2022-09-27 17:44:04 +0300

    + menu fade

--- script.js
+++ script.js
@@ -105,3 +105,28 @@ tabsContainer.addEventListener('click', (e) => {
     document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 
 })
+
+// menu fade
+const handleHover = function (e, opacity){
+    if (e.target.classList.contains('nav__link')){
+        const link = e.target;
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link')
+        const logo = link.closest('.nav').querySelector('img')
+
+        siblings.forEach(el=>{
+            if (el!== link) el.style.opacity = opacity
+        })
+        logo.style.opacity= opacity
+    }
+}
+
+
+const nav = document.querySelector('.nav');
+
+nav.addEventListener('mouseover', function (e){handleHover(e, 0.5)});
+
+nav.addEventListener('mouseout', function (e){handleHover(e, 1)});
+
+// we can use bind
+// const handleHover = function (e)
+// nav.addEventListener('mouseover', handleHover.bind(1) );
